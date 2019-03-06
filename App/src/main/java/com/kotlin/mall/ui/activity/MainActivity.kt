b5cdaa05e0369c1f67bf5249977a04589e0c97ec
pengllrn <1256880407@qq.com>
2019-03-06 19:12:04 +0800

    Fragment switch

--- App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.kotlin.mall.ui.activity
 
 import android.os.Bundle
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
 import com.kotlin.base.ui.activity.BaseActivity
+import com.kotlin.base.ui.fragment.BaseFragment
 import com.kotlin.mall.R
 import com.kotlin.mall.ui.fragment.HomeFragment
+import com.kotlin.mall.ui.fragment.MeFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import rx.Observable
-import rx.android.schedulers.AndroidSchedulers
-import java.util.concurrent.TimeUnit
 
 /**
  * Author：pengllrn
  * Time: 2019/3/5 23:12
  */
 class MainActivity:BaseActivity() {
+    private val mList = ArrayList<BaseFragment>(5)
+    private val mHomeFragment by lazy { HomeFragment() }
+    private val mCategoryFragment by lazy { HomeFragment() }
+    private val mCartFragment by lazy { HomeFragment() }
+    private val mMsgFragment by lazy { HomeFragment() }
+    private val mMeFragment by lazy { MeFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,16 +28,53 @@ class MainActivity:BaseActivity() {
         mBottomNavBar.checkMsgBadge(false)
         mBottomNavBar.checkCartBadge(20)
 
-        Observable.timer(2, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({//2秒之后执行的操作
-                    mBottomNavBar.checkMsgBadge(true)})
-        initView()
+//        Observable.timer(2, TimeUnit.SECONDS)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe({//2秒之后执行的操作
+//                    mBottomNavBar.checkMsgBadge(true)})
+        initFragment()
+        initBottomNav()
+        changgeFragment(0)
     }
 
-    private fun initView(){
+    private fun initFragment() {
         val manager = supportFragmentManager.beginTransaction()
-        manager.replace(R.id.mContainer, HomeFragment())
+        manager.add(R.id.mContainer,mHomeFragment)
+        manager.add(R.id.mContainer,mCategoryFragment)
+        manager.add(R.id.mContainer,mCartFragment)
+        manager.add(R.id.mContainer,mMsgFragment)
+        manager.add(R.id.mContainer,mMeFragment)
+        manager.commit()
+
+        mList.add(mHomeFragment)
+        mList.add(mCategoryFragment)
+        mList.add(mCartFragment)
+        mList.add(mMsgFragment)
+        mList.add(mMeFragment)
+    }
+
+    private fun initBottomNav(){
+        mBottomNavBar.setTabSelectedListener(object :BottomNavigationBar.OnTabSelectedListener{
+            override fun onTabReselected(position: Int) {
+
+            }
+
+            override fun onTabUnselected(position: Int) {
+            }
+
+            override fun onTabSelected(position: Int) {
+                changgeFragment(position)
+            }
+
+        })
+    }
+
+    private fun changgeFragment(position: Int) {
+        val manager = supportFragmentManager.beginTransaction()
+        mList.forEach {
+            manager.hide(it)
+        }
+        manager.show(mList.get(position))
         manager.commit()
     }
 }
---
+++ App/src/main/java/com/kotlin/mall/ui/fragment/MeFragment.kt
@@ -0,0 +1,19 @@
+package com.kotlin.mall.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kotlin.base.ui.fragment.BaseFragment
+import com.kotlin.mall.R
+
+/**
+ * Author：pengllrn
+ * Time: 2019/3/6 18:48
+ */
+class MeFragment :BaseFragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_me,null)
+    }
+}
