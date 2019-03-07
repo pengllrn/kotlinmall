19e7bed0c91f0081760ed9a4d17c4785276515a1
pengllrn <1256880407@qq.com>
2019-03-07 08:54:53 +0800

    initial Category Center

---
+++ GoodsCenter/src/main/java/com/pengllrn/goods/presenter/CategoryPresenter.kt
@@ -0,0 +1,8 @@
+package com.pengllrn.goods.presenter
+
+/**
+ * Author：pengllrn
+ * Time: 2019/3/7 8:53
+ */
+class CategoryPresenter {
+}
---
+++ GoodsCenter/src/main/java/com/pengllrn/goods/presenter/view/CategoryView.kt
@@ -0,0 +1,8 @@
+package com.pengllrn.goods.presenter.view
+
+/**
+ * Author：pengllrn
+ * Time: 2019/3/7 8:54
+ */
+interface CategoryView {
+}
